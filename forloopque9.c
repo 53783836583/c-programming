f0679671a2c00bda05904d3a9833d8f2981086db
Adhithya Raja <adhithyaraja75@gmail.com>
2026-02-15 16:49:51 +0530

    Add files via upload
    day 5

---
+++ forloopque9.c
@@ -0,0 +1,14 @@
+//
+// Created by adhit on 15-02-2026.
+//
+#include <stdio.h>
+int main () {
+    int count;
+    int n;
+    scanf("%d", &n);
+    for (int i;; i++) {
+       count=count+i;
+        printf ("%d\n",i);
+        return 0;
+    }
+}
