683d89cad16fcbdcbdbc40982a27cb4f0bbe7f0f
Adhithya Raja <adhithyaraja75@gmail.com>
2026-02-22 16:58:14 +0530

    Add files via upload
    day 6

--- forloopque5.c
+++ forloopque5.c
@@ -3,8 +3,10 @@
 //
 #include <stdio.h>
 int main () {;
-    for (int i = 10; i <= 0; i++) {
-        printf("%d ",i);
+    int n;
+    scanf("%d", &n);
+    for (int i=n;i>0;i--) {
+    printf("%d ",i);
     }
     return 0;
 }
