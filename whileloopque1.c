b3dba72bb613ea8fb594813dd290656300810c85
Adhithya Raja <adhithyaraja75@gmail.com>
2026-02-27 09:59:50 +0530

    Add files via upload
    day 7

---
+++ whileloopque1.c
@@ -0,0 +1,28 @@
+//
+// Created by adhit on 21-02-2026.
+//
+#include<stdio.h>
+int main() {
+    int nv;
+    int lvs;
+    int count=0;
+    int max=0,b=0;
+    scanf("%d",&nv);
+    int i=1;
+    while(i<=nv) {
+        scanf("%d",&lvs);
+        if(lvs>70) {
+            count++;
+            max++;
+            if (max>b)
+                b=max;
+        }
+        else {
+            max=0;
+        }
+        i++;
+    }
+    printf("Noise Violation: %d\n",count);
+    printf("Longest Violation streak: %d\n",b);
+    return 0;
+}
