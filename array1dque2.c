d43a5cc18c002445132ada69a704e4d4d42f7478
Adhithya Raja <adhithyaraja75@gmail.com>
2026-03-01 17:34:33 +0530

    Add files via upload
    day 8

---
+++ array1dque2.c
@@ -0,0 +1,22 @@
+//
+// Created by adhit on 27-02-2026.
+//
+#include <ctype.h>
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d",&size);
+    int m;
+    scanf("%d",&m);
+    int count=0;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < size; i++) {
+        if (m==arr[i]) {
+            count++;
+        }
+    }
+    printf("%d\n",count);
+}
