115e0ed43971a0e4cdc9b333b864f5da7049cdd6
Adhithya Raja <adhithyaraja75@gmail.com>
2026-03-04 19:34:13 +0530

    Add files via upload
    day 8

---
+++ array1dque5.c
@@ -0,0 +1,19 @@
+//
+// Created by adhit on 27-02-2026.
+//
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int sum=0;
+    int act=0;
+    for(int i = 0; i < size; i++) {
+        sum+=arr[i];
+        act+=i+1;
+    }
+    printf("%d", act-sum);
+}
---
+++ array1dque8.c
@@ -0,0 +1,24 @@
+//
+// Created by adhit on 27-02-2026.
+//
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int k;
+    scanf("%d", &k);
+    for (int i=0;i<k;i++) {
+        int temp=arr[0];
+        for (int j=0;j<size-1;j++) {
+            arr[j]=arr[j+1];
+        }
+        arr[size-1]=temp;
+    }
+    for (int i=0;i<size;i++) {
+        printf("%d ", arr[i]);
+    }
+}
