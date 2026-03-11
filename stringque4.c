0abe44c86902b98f6c54b6ed97cb85e0dadb6c79
Adhithya Raja <adhithyaraja75@gmail.com>
2026-03-11 20:31:53 +0530

    Add files via upload
    day 10

---
+++ stringque4.c
@@ -0,0 +1,17 @@
+//
+// Created by adhit on 09-03-2026.
+//
+#include<stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int len=strlen(str);
+    char feed[len+1];
+    int j=0;
+    for (int i=len-2;i>=0;i--) {
+        feed[j++]=str[i];
+    }
+    printf("%s",feed);
+}
