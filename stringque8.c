4537e8d836f333e5b0a0e0870e73c5b6b27cb234
Adhithya Raja <adhithyaraja75@gmail.com>
2026-03-12 19:15:07 +0530

    Add files via upload
    day 10

---
+++ stringque8.c
@@ -0,0 +1,17 @@
+//
+// Created by adhit on 09-03-2026.
+//
+#include<stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int len=strlen(str);
+    int count=0;
+    for (int i=0;i<len-1;i++) {
+        if (str[i]==' ') {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
