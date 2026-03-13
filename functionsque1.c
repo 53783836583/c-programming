56fa14cb4cb2ab0f10bd093670bcb0cd80ed4898
Adhithya Raja <adhithyaraja75@gmail.com>
2026-03-13 19:55:47 +0530

    Add files via upload
    day 11

---
+++ functionsque1.c
@@ -0,0 +1,13 @@
+//
+// Created by adhit on 10-03-2026.
+//
+#include <stdio.h>
+float simple_interest(int a, int b, int c) {
+    return (a*b*c)/100;
+}
+int main() {
+    int p,r,t;
+    scanf("%d %d %d",&p,&r,&t);
+    float interst=simple_interest(p,r,t);
+    printf("%.2f",interst);
+}
